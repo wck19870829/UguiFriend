f20c025e5f05a540d0eda890d152c4672b7cbdaf
wuchaokun <164948850@qq.com>
2018-10-11 16:47:05 +0800

    暂存

--- Script/Core/UguiWrapContent.cs
+++ Script/Core/UguiWrapContent.cs
@@ -6,42 +6,92 @@ using System;
 
 namespace RedScarf.UguiFriend
 {
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(ScrollRect))]
     /// <summary>
-    /// 元素循环
+    /// 元素循环容器
     /// </summary>
     public class UguiWrapContent : MonoBehaviour
     {
-        public float spacing = 100;
-        [SerializeField] protected ScrollRect scrollRect;
-        [SerializeField] protected Axis axis;
-        protected GridLayoutGroup gridLayoutGroup;
+        [SerializeField]protected float spacing = 100;
+        [SerializeField]protected Axis axis;
+        protected ScrollRect scrollRect;
+        protected Mask mask;
         protected List<RectTransform> items;
+        protected Vector3[] maskCorners;
+        protected bool firstTime;
 
         protected virtual void Start()
         {
             if (scrollRect == null)
+                scrollRect = GetComponentInChildren<ScrollRect>();
+            if (scrollRect == null)
                 throw new Exception("Scroll rect is null!");
-            if (scrollRect.content==null)
+            if (scrollRect.content == null)
                 throw new Exception("Scroll rect content is null!");
 
+            var fitter = scrollRect.content.GetComponent<ContentSizeFitter>();
+            if (fitter == null)
+                fitter = scrollRect.content.gameObject.AddComponent<ContentSizeFitter>();
+            fitter.horizontalFit = ContentSizeFitter.FitMode.MinSize;
+            fitter.verticalFit = ContentSizeFitter.FitMode.MinSize;
+
+            mask = scrollRect.GetComponentInChildren<Mask>();
+            if (mask == null)
+                throw new Exception("Mask is null!");
+
+            maskCorners = new Vector3[4];
             items = new List<RectTransform>();
             for (var i=0;i< scrollRect.content.childCount;i++)
             {
                 items.Add(scrollRect.content.GetChild(i) as RectTransform);
             }
+
+            firstTime = true;
+            WrapContent();
+            firstTime = false;
+#if UNITY_EDITOR
+            if (!UnityEditor.EditorApplication.isPlaying)
+            {
+                firstTime = true;
+            }
+#endif
         }
 
         private void Update()
         {
-            if (scrollRect == null) return;
-            if (scrollRect.content == null) return;
+            WrapContent();
+        }
 
+        protected virtual void WrapContent()
+        {
+            if (scrollRect == null || scrollRect.content == null||mask==null) return;
+
+            mask.rectTransform.GetWorldCorners(maskCorners);
+            var center = Vector3.Lerp(maskCorners[0], maskCorners[2], 0.5f);
+            var localCenter = scrollRect.content.InverseTransformPoint(center);
+            var extents = spacing * items.Count * 0.5f;
+            var ext2 = extents * 2f;
             if (axis == Axis.Vertical)
             {
-                for (var i=0;i<items.Count;i++)
+                for (var i = 0; i < items.Count; i++)
                 {
                     var item = items[i];
-
+                    var localPos = item.localPosition;
+                    var distance = localPos.y - localCenter.y;
+                    if (firstTime)
+                    {
+                        localPos = new Vector3(localCenter.x, -i * spacing-spacing*0.5f);
+                    }
+                    else if (distance < -extents)
+                    {
+                        localPos.y += ext2;
+                    }
+                    else if (distance > extents)
+                    {
+                        localPos.y -= ext2;
+                    }
+                    item.localPosition = localPos;
                 }
             }
             else
@@ -49,14 +99,25 @@ namespace RedScarf.UguiFriend
                 for (var i = 0; i < items.Count; i++)
                 {
                     var item = items[i];
-
+                    var localPos = item.localPosition;
+                    var distance = localPos.x - localCenter.x;
+                    if (firstTime)
+                    {
+                        localPos = new Vector3(i * spacing+spacing * 0.5f, localCenter.y);
+                    }
+                    else if (distance < -extents)
+                    {
+                        localPos.x += ext2;
+                    }
+                    else if (distance > extents)
+                    {
+                        localPos.x -= ext2;
+                    }
+                    item.localPosition = localPos;
                 }
             }
         }
 
-        /// <summary>
-        /// 方向
-        /// </summary>
         public enum Axis
         {
             Horizontal,
