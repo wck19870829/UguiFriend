3fa971a17af54f14f8f5b5fb5ee236d63bb15f4a
wck19870829 <164948850@qq.com>
2018-11-08 01:17:30 +0800

    暂存

---
+++ Script/Core/UguiBezierLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace RedScarf.UguiFriend
+{
+    /// <summary>
+    /// 贝塞尔曲线
+    /// </summary>
+    public class UguiBezierLine : UIBehaviour
+    {
+
+    }
+
+    /// <summary>
+    /// 线段分段
+    /// </summary>
+    public class UguiBezierLineSegment : ScriptableObject
+    {
+        public Vector3 start;
+        public Vector3 end;
+        public Color color;
+    }
+}
---
+++ Script/Core/UguiLine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace RedScarf.UguiFriend
+{
+    /// <summary>
+    /// 线段
+    /// </summary>
+    public class UguiLine : Graphic
+    {
+        protected virtual void DrawLine()
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// 线段分段
+    /// </summary>
+    public class UguiLineSegment : ScriptableObject
+    {
+        public Vector3 start;
+        public Vector3 end;
+        public Color color;
+    }
+}
