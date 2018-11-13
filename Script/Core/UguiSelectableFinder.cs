48c2dc28a01fc4b5f2e0abb0e6e5fa535820f53c
wck19870829 <164948850@qq.com>
2018-11-13 23:51:15 +0800

    暂存

---
+++ Script/Core/UguiSelectableFinder.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace RedScarf.UguiFriend
+{
+    /// <summary>
+    /// 查找交互物体
+    /// </summary>
+    public class UguiSelectableFinder : UIBehaviour
+    {
+        [SerializeField] protected RectTransform m_Children;
+        protected RectTransform m_CurrentTarget;
+
+        /// <summary>
+        /// 查找下一个
+        /// </summary>
+        /// <param name="direction">查找方向</param>
+        /// <returns></returns>
+        public RectTransform FindNext(FindDirection direction)
+        {
+
+
+            return null;
+        }
+
+        /// <summary>
+        /// 设置当前目标
+        /// </summary>
+        public RectTransform CurrentTarget
+        {
+            get
+            {
+                return m_CurrentTarget;
+            }
+            set
+            {
+                m_CurrentTarget = value;
+            }
+        }
+
+        public enum FindDirection
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+    }
+}
---
+++ Script/Interaction/GraphicsTablet/UguiGraphicsTablet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace RedScarf.UguiFriend
+{
+    /// <summary>
+    /// 手绘板
+    /// </summary>
+    public class UguiGraphicsTablet : UIBehaviour
+    {
+
+    }
+}
