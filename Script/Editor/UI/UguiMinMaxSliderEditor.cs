a5b0d494ced754abfff4eab960eba9c8df2837c0
wuchaokun <wuchaokun@shuiqinling.com>
2019-01-03 17:44:12 +0800

    暂存

--- Script/Editor/UI/UguiMinMaxSliderEditor.cs
+++ Script/Editor/UI/UguiMinMaxSliderEditor.cs
@@ -18,17 +18,8 @@ namespace RedScarf.UguiFriend
             var sliderBlockB=serializedObject.FindProperty("m_SliderBlockB");
             EditorGUILayout.PropertyField(sliderBlockB);
 
-            var limitLineStart = serializedObject.FindProperty("m_LimitLineStart");
-            EditorGUILayout.PropertyField(limitLineStart);
-
-            var limitLineEnd = serializedObject.FindProperty("m_LimitLineEnd");
-            EditorGUILayout.PropertyField(limitLineEnd);
-
-            var fillRect = serializedObject.FindProperty("m_FillRect");
-            EditorGUILayout.PropertyField(fillRect);
-
-            var handleRect=serializedObject.FindProperty("m_HandleRect");
-            EditorGUILayout.PropertyField(handleRect);
+            var handleSlideArea = serializedObject.FindProperty("m_HandleSlideArea");
+            EditorGUILayout.PropertyField(handleSlideArea);
 
             var minValue=serializedObject.FindProperty("m_MinValue");
             EditorGUILayout.PropertyField(minValue);
--- Script/Interaction/MinMaxSlider/UguiMinMaxSlider.cs
+++ Script/Interaction/MinMaxSlider/UguiMinMaxSlider.cs
@@ -9,7 +9,7 @@ namespace RedScarf.UguiFriend
     /// <summary>
     /// 区间滑块
     /// </summary>
-    public class UguiMinMaxSlider : Selectable, 
+    public class UguiMinMaxSlider : Selectable,
         IEventSystemHandler,
         IInitializePotentialDragHandler,
         IDragHandler,
@@ -17,22 +17,29 @@ namespace RedScarf.UguiFriend
     {
         [SerializeField] protected RectTransform m_SliderBlockA;
         [SerializeField] protected RectTransform m_SliderBlockB;
-        [SerializeField] protected RectTransform m_LimitLineStart;
-        [SerializeField] protected RectTransform m_LimitLineEnd;
-        [SerializeField] protected RectTransform m_FillRect;
-        [SerializeField] protected RectTransform m_HandleRect;
+        [SerializeField] protected RectTransform m_HandleSlideArea;
         [SerializeField] protected float m_MinValue;
         [SerializeField] protected float m_MaxValue;
+        [SerializeField] protected float m_MinLimit;
+        [SerializeField] protected float m_MaxLimit;
         [SerializeField] protected bool m_WholeNUmbers;
+        [SerializeField] protected Direction m_Direction;
 
         protected RectTransform currentTarget;
 
-        public Action<float, float> OnValueChange;
+        public Action OnValueChange;
 
         protected UguiMinMaxSlider()
         {
-            m_MaxValue = 10;
+            m_MinLimit = m_MaxValue = 10;
             m_WholeNUmbers = true;
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Set(m_MinValue, m_MaxValue, m_MinLimit, m_MaxLimit);
         }
 
         public void GraphicUpdateComplete()
@@ -76,23 +83,95 @@ namespace RedScarf.UguiFriend
         protected void UpdateBlockPosition(PointerEventData eventData)
         {
             if (currentTarget == null) return;
-            if (m_LimitLineStart == null || m_LimitLineEnd == null) return;
 
-            var limitPointStart = RectTransformUtility.WorldToScreenPoint(eventData.pressEventCamera, m_LimitLineStart.position);
-            var limitPointEnd = RectTransformUtility.WorldToScreenPoint(eventData.pressEventCamera, m_LimitLineEnd.position);
-            var screenLimitLine = new UguiMathf.Line2(limitPointStart, limitPointEnd);
-            var screenPoint = screenLimitLine.ProjectPoint(eventData.position);
-
-            Vector3 worldPos;
-            if(RectTransformUtility.ScreenPointToWorldPointInRectangle(transform as RectTransform, screenPoint, eventData.pressEventCamera, out worldPos))
+            var localPoint = Vector2.zero;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(m_HandleSlideArea, eventData.position, eventData.pressEventCamera, out localPoint))
             {
-                currentTarget.position = worldPos;
+                var minValue = m_MinValue;
+                var maxValue = m_MaxValue;
+                minValue=maxValue=localPoint[(int)m_Direction];
 
-                if (OnValueChange != null)
-                {
-                    //OnValueChange.Invoke();
-                }
+                Set(minValue,maxValue);
             }
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        public void Set(float minValue, float maxValue)
+        {
+            Set(minValue, maxValue, m_MinLimit, m_MaxLimit);
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <param name="minLimit"></param>
+        /// <param name="maxLimit"></param>
+        public void Set(float minValue, float maxValue, float minLimit, float maxLimit)
+        {
+            minLimit = Mathf.Min(minLimit, maxLimit);
+            minValue = Mathf.Clamp(minValue, minLimit, maxLimit);
+            maxValue = Mathf.Clamp(maxValue, minLimit, maxLimit);
+            m_MinValue = minValue;
+            m_MaxValue = maxValue;
+            m_MinLimit = minLimit;
+            m_MaxLimit = maxLimit;
+
+            SetSliderPos(m_SliderBlockA, m_HandleSlideArea,m_MinValue);
+            SetSliderPos(m_SliderBlockB, m_HandleSlideArea, m_MaxValue);
+
+            if (OnValueChange != null)
+            {
+                OnValueChange.Invoke();
+            }
+        }
+
+        protected void SetSliderPos(RectTransform slider, RectTransform slideArea,float value)
+        {
+            if (slider == null || slideArea == null) return;
+
+            value = Mathf.Clamp(value, m_MinValue, m_MaxValue);
+            var percent = (value - m_MinLimit) / (m_MaxLimit - m_MinLimit);
+
+            var anchorMin = Vector2.zero;
+            var anchorMax = Vector2.one;
+            anchorMin[(int)m_Direction] = anchorMax[(int)m_Direction] = percent;
+            slider.anchorMin = anchorMin;
+            slider.anchorMax = anchorMax;
+        }
+
+        /// <summary>
+        /// 最小值
+        /// </summary>
+        public float MinValue { get { return m_MinValue; } }
+
+        /// <summary>
+        /// 最大值
+        /// </summary>
+        public float MaxValue { get { return m_MaxValue; } }
+
+        /// <summary>
+        /// 下限
+        /// </summary>
+        public float MinLimit { get { return m_MinLimit; } }
+
+        /// <summary>
+        /// 上限
+        /// </summary>
+        public float MaxLimit { get { return m_MaxLimit; } }
+
+        /// <summary>
+        /// 拖拽方向
+        /// </summary>
+        public enum Direction
+        {
+            Horizontal=0,
+            Vertical=1
         }
     }
 }
