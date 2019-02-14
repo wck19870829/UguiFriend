12e5219809bda15ddd1c0ddf1f6da907ac39f64a
wuchaokun <wuchaokun@shuiqinling.com>
2019-02-14 19:50:09 +0800

    暂存

--- Script/Interaction/ImageCropping/UguiImageCropper.cs
+++ Script/Interaction/ImageCropping/UguiImageCropper.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace RedScarf.UguiFriend
 {
@@ -12,12 +13,26 @@ namespace RedScarf.UguiFriend
     {
         [SerializeField] protected RawImage srcImage;               //待处理的图
         [SerializeField] protected Image safeFrame;                 //安全框
-        [SerializeField] protected RawImage maskImage;              //黑色遮罩
+        [SerializeField] protected RawImage maskImage;              //遮罩
+        [SerializeField] protected Color maskColor;                 //遮罩颜色
         [SerializeField] protected bool showBisectrix;              //安全框显示等分线
+        [SerializeField] protected int bisectrixColumn = 2;         //安全框等分线行列数量
+        [SerializeField] protected int bisectrixRow = 2;
+        [SerializeField] protected Color bisectrixColor;            //安全框等分线颜色
+
+        protected List<RawImage> bisectrixColumnList;
+        protected List<RawImage> bisectrixRowList;
+        protected List<EventTrigger> safeFrameDragList;
+        protected Canvas m_Canvas;
 
         protected UguiImageCropper()
         {
             showBisectrix = true;
+            maskColor = new Color(0,0,0,0.7f);
+            bisectrixColor = new Color(1,1,1,0.3f);
+            bisectrixColumnList = new List<RawImage>();
+            bisectrixRowList = new List<RawImage>();
+            safeFrameDragList = new List<EventTrigger>();
         }
 
         protected override void Awake()
@@ -26,6 +41,40 @@ namespace RedScarf.UguiFriend
 
             safeFrame.type = Image.Type.Sliced;
             safeFrame.raycastTarget = false;
+            for (var i=0;i<4;i++)
+            {
+                var dragButton = UguiTools.AddChild<RawImage>("DragButton",safeFrame.transform);
+                dragButton.rectTransform.sizeDelta = new Vector2(60,60);
+                var dragButtonTigger = dragButton.gameObject.AddComponent<EventTrigger>();
+                var dragEntry = new EventTrigger.Entry();
+                dragEntry.eventID = EventTriggerType.Drag;
+                dragEntry.callback.AddListener(OnSafeFrameDrag);
+                dragButtonTigger.triggers.Add(dragEntry);
+            }
+
+            var srcTrigger=srcImage.gameObject.AddComponent<EventTrigger>();
+            var srcEntry = new EventTrigger.Entry();
+            srcEntry.eventID = EventTriggerType.Drag;
+            srcEntry.callback.AddListener(OnSrcImageDrag);
+            srcTrigger.triggers.Add(srcEntry);
+
+            //创建安全框等分线
+            for (var i=0;i< bisectrixColumn; i++)
+            {
+                var line = UguiTools.AddChild<RawImage>("Bisectrix",safeFrame.transform);
+                line.color = bisectrixColor;
+                line.raycastTarget = false;
+                bisectrixColumnList.Add(line);
+            }
+            for (var i = 0; i < bisectrixRow; i++)
+            {
+                var line = UguiTools.AddChild<RawImage>("Bisectrix", safeFrame.transform);
+                line.color = bisectrixColor;
+                line.raycastTarget = false;
+                bisectrixRowList.Add(line);
+            }
+
+            m_Canvas = GetComponentInParent<Canvas>();
         }
 
         protected virtual void Update()
@@ -47,9 +96,54 @@ namespace RedScarf.UguiFriend
                 var yMax = (safeFrameRect.yMax - maskRect.yMin) / Mathf.Abs(maskRect.height);
                 var safeFrameValue = new Vector4(xMin,xMax,yMin,yMax);
                 maskImage.material.SetVector("_SafeFrame", safeFrameValue);
+            }
 
-                //更新等分线
+            UpdateBisectrix();
+        }
 
+        /// <summary>
+        /// 更新等分线
+        /// </summary>
+        protected virtual void UpdateBisectrix()
+        {
+            if (safeFrame)
+            {
+                for (var i = 0; i < bisectrixColumn; i++)
+                {
+                    var line = bisectrixColumnList[i];
+                    line.rectTransform.sizeDelta = new Vector2(safeFrame.rectTransform.rect.width, 1);
+                }
+                for (var i = 0; i < bisectrixRow; i++)
+                {
+                    var line = bisectrixRowList[i];
+                    line.rectTransform.sizeDelta = new Vector2(1, safeFrame.rectTransform.rect.height);
+                }
+            }
+        }
+
+        protected virtual void OnSrcImageDrag(BaseEventData eventData)
+        {
+            PointerEventData pe = eventData as PointerEventData;
+            if (pe != null)
+            {
+                Vector3 worldPoint;
+                if (RectTransformUtility.ScreenPointToWorldPointInRectangle(safeFrame.rectTransform, pe.position, m_Canvas.rootCanvas.worldCamera, out worldPoint))
+                {
+                    pe.pointerDrag.transform.position = worldPoint;
+                }
+            }
+        }
+
+        protected virtual void OnSafeFrameDrag(BaseEventData eventData)
+        {
+            PointerEventData pe = eventData as PointerEventData;
+            if (pe!=null)
+            {
+                Vector3 worldPoint;
+                if(RectTransformUtility.ScreenPointToWorldPointInRectangle(safeFrame.rectTransform, pe.position, m_Canvas.rootCanvas.worldCamera, out worldPoint))
+                {
+                    pe.pointerDrag.transform.position = worldPoint;
+                }
             }
         }
 
