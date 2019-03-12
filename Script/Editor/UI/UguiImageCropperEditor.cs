95a7d362a000bc9d11026865e01a9784c97e47f4
wuchaokun <wuchaokun@shuiqinling.com>
2019-03-12 18:43:04 +0800

    暂存

---
+++ Script/Editor/UI/UguiImageCropperEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace RedScarf.UguiFriend
+{
+    [CustomEditor(typeof(UguiImageCropper))]
+    public class UguiImageCropperEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+        }
+    }
+}
